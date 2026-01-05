6360d706043f8eeb01f2d5d66b174a6859128f5c
Roman Kosarev <rmksrv@outlook.com>
2026-01-05 11:25:16 +0400

    developing nix

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,43 +1,39 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs = { 
+      url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    };
+    darwin = {
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs; [
-        glances
-      ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Romans-MacBook-Pro
-    darwinConfigurations."Romans-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+  outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: {
+    darwinConfigurations = {
+      "Romans-MacBook-Pro" = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          {
+            system.stateVersion = 6;
+            nixpkgs.hostPlatform = "aarch64-darwin";
+            nix.settings.experimental-features = "nix-command flakes";
+          }
+          ./configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # home-manager.users.rmksrv = ./home.nix;
+          }
+        ];
+      };
     };
   };
 }
